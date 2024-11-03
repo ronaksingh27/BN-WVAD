fde5804d49f621e2374beaf6fb775edaee5a18f5
ronak <ronaksingh2708@gmail.com>
2024-11-03 11:01:53 +0530

    second commits

--- dataset_loader.py
+++ dataset_loader.py
@@ -30,13 +30,12 @@ class UCF_crime(data.DataLoader):
         return len(self.vid_list)
 
     def __getitem__(self, index):
-        
         if self.mode == "Test":
-            data,label,name = self.get_data(index)
-            return data,label,name
+            data, label, name = self.get_data(index)
+            return data, label, name
         else:
-            data,label = self.get_data(index)
-            return data,label
+            data, label = self.get_data(index)
+            return data, label
 
     def get_data(self, index):
         vid_info = self.vid_list[index][0]  
